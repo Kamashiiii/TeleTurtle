6d3d8c01e04e9226d92a55a5abdd6e695b7b9826
RichardGaming772 <97022820+RichardGaming772@users.noreply.github.com>
2022-06-23 09:22:40 +0200

    TeleTurtle 0.1.1
    La tortue peut marcher et rentrer dans sa carapace

---
+++ Assets/animationStateController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animationStateController : MonoBehaviour
+{
+    Animator animator;
+
+    public float hideCooldown;
+    float hide;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("z"))
+        {
+            animator.SetBool("isWalking", true);
+        }
+        else
+            animator.SetBool("isWalking", false);
+        if (Input.GetKey("g"))
+        {
+
+            if (!animator.GetBool("isHiding"))
+            {
+                if (Time.time - hide < hideCooldown)
+                    return;
+                animator.SetBool("isHiding", true);
+                hide = Time.time;
+            }
+            else
+            {
+                if (Time.time - hide < hideCooldown)
+                    return;
+                animator.SetBool("isHiding", false);
+                hide = Time.time;
+            }
+        }
+    }
+}
