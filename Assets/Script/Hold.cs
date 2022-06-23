96af1705ced1df21b948eb831370b6e769b088d6
RichardGaming772 <killian.kvella@gmail.com>
2022-06-23 22:52:28 +0200

    TeleTurtle V0.2.2.1.1
    Ajout de script (non testé) Hold pour ramasser la tortue

---
+++ Assets/Script/Hold.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hold : MonoBehaviour
+{
+    public Transform holder;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!this.animationStateController.selected)
+        {
+            if (Input.GetMouseButtonDown(0))
+                place();
+            if (Input.GetMouseButtonDown(0))
+                pickup();
+        }
+    }
+
+    void pickup()
+    {
+        GetComponent<RigidBody>().useGravity = false;
+        this.transform.position = holder.position;
+        this.transform.parent = GameObject.Find("Holder");
+    }
+    void place()
+    {
+        this.transform.parent = null;
+        GetComponent<RigidBody>().useGravity = true;
+    }
+}
