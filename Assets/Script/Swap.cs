e6fc74ccb9600d7113976f3bf7b0094757d03958
Kentokku <bardotti.q@gmail.com>
2022-06-24 09:17:38 +0200

    Menu
    oUi menu qui marche

--- Assets/Script/Swap.cs
+++ Assets/Script/Swap.cs
@@ -20,7 +20,7 @@ public class Swap : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        selectedTurtle = "rouge";
+        selectedTurtle = "bleu";
         cameraBleu.SetActive(false);
         cameraRouge.SetActive(true);
 
