3bae2016aae972d8a952f5103a57bdb7f1a108aa
Kamashiiii <59314496+Kamashiiii@users.noreply.github.com>
2022-06-24 11:21:25 +0200

    TeleTurtle 0.4
    Changement de scene du level 1 au 2

---
+++ Assets/NextLevel.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class NextLevel : MonoBehaviour
+{
+    
+
+
+    void Start()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        switch (other.gameObject.tag)
+        {
+            case "Beet":
+                LoadLevel();
+                break;
+        }
+    }
+
+    void LoadLevel()
+    {
+        //SceneManager.LoadScene(LevelToLoad);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
