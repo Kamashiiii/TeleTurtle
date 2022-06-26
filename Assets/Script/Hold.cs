c7a90a9bd63a6df709129b46384fd991779eca78
RichardGaming772 <killian.kvella@gmail.com>
2022-06-26 14:35:34 +0200

    TeleTurtle V0.4
    Ajout dans lancer avec force et de bar de lancer

--- Assets/Script/Hold.cs
+++ Assets/Script/Hold.cs
@@ -2,40 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.Characters.ThirdPerson;
+using UnityEngine.UI;
 
 public class Hold : MonoBehaviour
 {
     public Transform holder;
     public bool isHeld;
-    public float throwForce;
+    public float minForce;
     bool charge;
     public float maxForce;
     public float chargeSpeed;
+    float throwForce;
+    public Slider slider;
+
+    void Start()
+    {
+        charge = false;
+        throwForce = minForce - 0.1f;
+        slider.maxValue = maxForce;
+        slider.minValue = throwForce = minForce - 0.1f; ;
+    }
 
     // Update is called once per frame
     void Update()
     {
         if (!GetComponent<animationStateController>().selected)
         {
-            if (Input.GetMouseButtonDown(0) && isHeld)
-                turtleThrow();
             if (Input.GetMouseButtonDown(1))
                 pickup();
+            if (Input.GetMouseButtonDown(0) && isHeld)
+                charge = true;
+            if (Input.GetMouseButtonUp(0))
+                charge = false;
+            if (charge && throwForce <= maxForce)
+                throwForce += chargeSpeed;
+            if (!charge && throwForce >= minForce && isHeld)
+                turtleThrow();
+            slider.value = throwForce;
         }
-    }
-
-    void buttonHold()
-    {
-        charge = true;
-    }
-
-    void buttonRelease()
-    {
-        charge = false;
     }
 
     void pickup()
     {
+        GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
         GetComponent<ThirdPersonCharacter>().m_IsGrounded = false;
         GetComponent<ThirdPersonUserControl>().enabled = false;
         GetComponent<animationStateController>().enabled = false;
@@ -52,5 +61,7 @@ public class Hold : MonoBehaviour
         GetComponent<Rigidbody>().useGravity = true;
         GetComponent<Rigidbody>().AddForce((holder.forward + new Vector3(0, 0.5f, 0)) * throwForce, ForceMode.Impulse);
         isHeld = false;
+        charge = false;
+        throwForce = minForce - 0.1f;
     }
 }
