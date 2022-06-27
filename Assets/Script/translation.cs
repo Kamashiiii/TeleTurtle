6f0e36516f81beac527afed2e8f436c1fb654442
RichardGaming772 <killian.kvella@gmail.com>
2022-06-27 13:05:27 +0200

    le script est fix
    derien theo

---
+++ Assets/Script/translation.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class translation : MonoBehaviour
+{
+
+    public new Transform transform;
+    Vector3 startPos;
+    Vector3 pos;
+    public bool left = true;
+    public bool right = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pos = transform.position;
+        if (transform.position.x <= 16.75f && right)
+        {
+            transform.position = new Vector3(pos.x + 0.01f, startPos.y, startPos.z);
+            left = false;
+        }
+        else
+            left = true;
+
+        if (transform.position.x >= -0.75f && left)
+        {
+            transform.position = new Vector3(pos.x - 0.01f, startPos.y, startPos.z);
+            right = false;
+        }
+        else
+            right = true;
+
+    }
+}
