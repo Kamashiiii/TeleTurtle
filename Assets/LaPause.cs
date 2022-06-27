81972c586680f626f4688f94b2c425fe2ae10d37
Kentokku <bardotti.q@gmail.com>
2022-06-27 11:37:54 +0200

    menu test
    marche po

---
+++ Assets/LaPause.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LaPause : MonoBehaviour
+{
+    public static bool JeuEnPause = false;
+    public GameObject MenuPauseUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(JeuEnPause)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+        
+    }
+    public void Resume()
+    {
+        MenuPauseUI.SetActive(false);
+        Time.timeScale = 1f;
+        JeuEnPause = false;
+    }
+    public void Pause()
+    {
+        MenuPauseUI.SetActive(true);
+        Time.timeScale = 0f;
+        JeuEnPause = true;
+    }
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
