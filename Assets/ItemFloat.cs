4d53d0a719f9f3b0d98e9b688a970a22ae8f35cb
RichardGaming772 <killian.kvella@gmail.com>
2022-06-27 22:51:53 +0200

    V0.4.5
    Ajout Salle 4 (sans textures)

--- Assets/ItemFloat.cs
+++ Assets/ItemFloat.cs
@@ -11,36 +11,20 @@ public class ItemFloat : MonoBehaviour
     float yRotate;
     float rotation;
 
-    /*
-    public int y2;
-    public bool monte;*/
-
 
     // Start is called before the first frame update
     void Start()
     {
         x = aliment.transform.position.x;
         y = aliment.transform.position.y;
-        z = aliment.transform.position.z;/*
+        z = aliment.transform.position.z;
 
-        y2 = 0;
-        monte = false;*/
+
     }
 
     // Update is called once per frame
     void Update()
-    {/*
-        if (y2 == 0)
-            monte = true;
-        else if (y2 == 20)
-            monte = false;
-
-        if (monte)
-            y2 += 1;
-        else 
-            y2 -= 1;
-
-        aliment.transform.position = new Vector3 ((float)x, (float)y + y2, (float)z);*/
+    {
         float timer = Time.time;
 
         aliment.transform.position = new Vector3(x, y + (Mathf.Sin(3*timer)/2), z);
