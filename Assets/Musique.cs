cbe3e6399cc72ed1ef7529d6adcd8d5669d04521
TBBT2003 <tbouvier2003@gmail.com>
2022-06-28 16:43:45 +0200

    sdfgsdfg
    lskdfng

---
+++ Assets/Musique.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Musique : MonoBehaviour
+{
+    Object.DontDestroyOnLoad
+
+    public AudioSource sourceAudio;
+
+    [SerializeField] AudioClip musiqueDeFond;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sourceAudio = GetComponent<AudioSource>();
+        sourceAudio.clip = musiqueDeFond;
+        sourceAudio.Play();          
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
